34f260e4a6414130a2edd58e07eb0b70bc56a539
Daniel de Oquiñena <daniel@fast2.se>
2022-09-20 06:44:24 +0200

    Keep scratchpad ontop

--- rc.lua
+++ rc.lua
@@ -181,6 +181,7 @@ local term_scratch = bling.module.scratchpad {
   sticky                  = true, -- Whether the scratchpad should be sticky
   autoclose               = false, -- Whether it should hide itself when losing focus
   floating                = true, -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
+  ontop                   = true,
   geometry                = { x = 360, y = 90, height = 900, width = 1200 }, -- The geometry in a floating state
   reapply                 = true, -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
   dont_focus_before_close = false, -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
